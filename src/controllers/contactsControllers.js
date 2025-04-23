0113812340aa004aaa1787413710361e007e197b
Andrey <kovratovskiy1999@gmail.com>
2025-04-23 15:26:36 -0400

    completed ls-2

---
+++ src/controllers/contactsControllers.js
@@ -0,0 +1,50 @@
+const { ctrlWrapper } = require('../helpers');
+const contactServices = require('../services/contactsServices');
+const { HttpError } = require('../helpers')
+
+
+const getAllContacts = async (req, res) => {
+            const contacts = await contactServices.listContacts()
+            res.status(200).json(contacts)        
+}
+
+const getOneContact = async (req, res) => {
+        const {id} = req.params
+        const contact = await contactServices.getContactById(id)
+        if(!contact) {
+           throw HttpError(404,'Contact not found')
+        }
+        res.status(200).json(contact)
+}
+
+const deleteContact = async (req, res) => {
+        const {id} = req.params
+        const contacts = await contactServices.removeContact(id)
+        if(!contacts) {
+            throw HttpError(404,'Contact not found')
+        }
+        res.json({message: 'Contact deleted'})
+}   
+
+const createContact = async (req, res) => {
+        const contact = await contactServices.addContact(req.body)
+        res.status(201).json(contact)
+}
+
+const updateContact = async (req, res) => {
+        const {id} = req.params
+        const contact = await contactServices.getContactById(id)
+        if(!contact) {
+            throw HttpError(404,'Contact not found')
+        }
+        const updatedContact = await contactServices.addContact(req.body)
+        res.status(200).json(updatedContact)
+}
+
+module.exports = {
+    getAllContacts: ctrlWrapper(getAllContacts),
+    getOneContact: ctrlWrapper(getOneContact),
+    deleteContact:  ctrlWrapper(deleteContact),
+    createContact: ctrlWrapper(createContact),
+    updateContact: ctrlWrapper(updateContact)
+}
---
+++ src/routes/contactsRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const contactsRouter = express.Router();
+const {validateBody} = require('../middlewares')
+const {schema} = require('../shemas/contactShemas')
+const ctrl = require('../controllers/contactsControllers')
+
+
+
+contactsRouter.get('/' ,ctrl.getAllContacts)
+
+contactsRouter.get('/:id' ,ctrl.getOneContact)
+
+contactsRouter.post('/' ,ctrl.createContact)
+
+contactsRouter.put('/:id' ,validateBody(schema) ,ctrl.updateContact)
+
+contactsRouter.delete('/:id' ,ctrl.deleteContact)
+
+module.exports = contactsRouter
