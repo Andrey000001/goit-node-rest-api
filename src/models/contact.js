5ac46c537685fdd34225b273554a97367d3efb61
Andrey <kovratovskiy1999@gmail.com>
2025-04-26 13:42:30 -0400

    fix

--- src/models/contact.js
+++ src/models/contact.js
@@ -1,27 +1,50 @@
 const { model, Schema } = require('mongoose');
+const { HttpError } = require('../helpers');
+const Joi = require('joi');
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Set name for contact'],
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const phoneRegex = /^\+?[0-9\s\-()]{7,}$/;
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Set name for contact'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Set email for contact'],
+      match: [emailRegex, 'Please enter a valid email (e.g., user@example.com)'],
+      unique: true,
+    },
+    phone: {
+      type: String,
+      required: [true, 'Set phone for contact'],
+      match: [phoneRegex, 'Phone number must contain digits and may include +, -, ()'],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, 'Set email for contact'],
-    match: [
-      /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-      'Введите корректный email (например, user@example.com)',
-    ],
+  {
+    versionKey: false,
+    timestamps: true,
   },
-  phone: {
-    type: String,
-    required: [true, 'Set phone for contact'],
-    match: [/^\+?[0-9\s\-()]{7,}$/, 'Please enter a valid phone number'],
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
+);
+
+// const addSchema = Joi.object({
+//   name: Joi.string().required(),
+//   email: Joi.string().email().required(),
+//   phone: Joi.string()
+//     .pattern(/^[0-9]+$/)
+//     .required(),
+//   favorite: Joi.boolean(),
+// });
+
+contactSchema.post('save', (error, data, next) => {
+  HttpError(400, error.message);
+  next(error);
 });
 
 const Contact = model('contact', contactSchema);
