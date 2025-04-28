48a594516fcd6ff7cca47f44efc70cf3608b7e51
Andrey <kovratovskiy1999@gmail.com>
2025-04-28 10:56:02 -0400

    put out src

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const app = require('../app');
+const mongoose = require('mongoose');
+const logger = require('../logger/logger');
+require('dotenv').config();
+const { DB_HOST, PORT = 3000 } = process.env;
+
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(PORT))
+  .catch((error) => {
+    logger.error('Error connecting to DB:', error);
+    process.exit(1);
+  });
